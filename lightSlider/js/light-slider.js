a78bb98de7629c27a9452e773a8f5acb9d0fddfd
Alex <alexbibig@gmail.com>
2020-05-24 21:33:26 +0300

    change dog photos

--- lightSlider/js/light-slider.js
+++ lightSlider/js/light-slider.js
@@ -1,84 +1,79 @@
 const defaultOptions = {
-    slidesWidth: 300,
-    slidesHeight: 200,
-    nav: true,
-    slides: 2,
-}
+  slidesWidth: 300,
+  slidesHeight: 200,
+  nav: true, // сделать кнопки невидимыми или видимыми
+  slides: 2, // количество картинок (типа отобраджается 1,2,3)
+};
 let shift = 0;
 
 let divs = document.querySelectorAll('div');
-divs.forEach((el) => el.lightSlider = lightSlider);
+divs.forEach((el) => (el.lightSlider = lightSlider));
 
 function lightSlider() {
-    transformHtmlSlider(this);
-    setStyle();
-};
+  transformHtmlSlider(this);
+  setStyle();
+}
 
 function nextSlide() {
-    let slidersWrapper = 
-        document.querySelector('.light_slider .sliders_wrapper');
-    let slidersWrapperWidth = slidersWrapper.offsetWidth;
-    if ( shift > -(slidersWrapperWidth
-        - defaultOptions.slidesWidth) ) {
-            shift -= defaultOptions.slidesWidth;
-    }
-    slidersWrapper.style.transform = `translateX(${shift}px)`;
+  let slidersWrapper = document.querySelector('.light_slider .sliders_wrapper');
+  let slidersWrapperWidth = slidersWrapper.offsetWidth;
+  if (shift > -(slidersWrapperWidth - defaultOptions.slidesWidth)) {
+    shift -= defaultOptions.slidesWidth;
+  }
+  slidersWrapper.style.transform = `translateX(${shift}px)`;
 }
 function prevSlide() {
-    let slidersWrapper = 
-        document.querySelector('.light_slider .sliders_wrapper');
-    let slidersWrapperWidth = slidersWrapper.offsetWidth;
-    if ( shift < 0 ) {
-            shift += defaultOptions.slidesWidth;
-    }
-    slidersWrapper.style.transform = `translateX(${shift}px)`;
+  let slidersWrapper = document.querySelector('.light_slider .sliders_wrapper');
+  let slidersWrapperWidth = slidersWrapper.offsetWidth;
+  if (shift < 0) {
+    shift += defaultOptions.slidesWidth;
+  }
+  slidersWrapper.style.transform = `translateX(${shift}px)`;
 }
 
 function transformHtmlSlider(mainSlider) {
-    mainSlider.classList.add('light_slider')
-    let slidesHtml = mainSlider.innerHTML;
+  mainSlider.classList.add('light_slider');
+  let slidesHtml = mainSlider.innerHTML;
 
-    mainSlider.innerHTML = `<div class="sliders_window">
+  mainSlider.innerHTML = `<div class="sliders_window">
         <div class="sliders_wrapper">${slidesHtml}</div>
     </div>`;
 
-    
-    let navBlock = document.createElement('div');
-    navBlock.className = 'nav_block';
+  let navBlock = document.createElement('div');
+  navBlock.className = 'nav_block';
 
-    let prevBtn = document.createElement('button');
-    prevBtn.className = 'btn prev';
-    prevBtn.innerHTML = '<';
-    prevBtn.addEventListener('click', prevSlide);
+  let prevBtn = document.createElement('button');
+  prevBtn.className = 'btn prev';
+  prevBtn.innerHTML = '<';
+  prevBtn.addEventListener('click', prevSlide);
 
-    let nextBtn = document.createElement('button');
-    nextBtn.className = 'btn next';
-    nextBtn.innerHTML = '>';
-    nextBtn.addEventListener('click', nextSlide);
+  let nextBtn = document.createElement('button');
+  nextBtn.className = 'btn next';
+  nextBtn.innerHTML = '>';
+  nextBtn.addEventListener('click', nextSlide);
 
-
-    navBlock.append(prevBtn);
-    navBlock.append(nextBtn);
-    mainSlider.append(navBlock);
+  navBlock.append(prevBtn);
+  navBlock.append(nextBtn);
+  mainSlider.append(navBlock);
 }
 
 function setStyle() {
-    let slidersWindow = document
-    .querySelector('.light_slider .sliders_window');
+  let slidersWindow = document.querySelector('.light_slider .sliders_window');
 
-    slidersWindow.style.width = `${defaultOptions.slidesWidth}px`;
-    slidersWindow.style.height = `${defaultOptions.slidesHeight}px`;
+  slidersWindow.style.width = `${defaultOptions.slidesWidth}px`;
+  slidersWindow.style.height = `${defaultOptions.slidesHeight}px`;
 
-    let slides = document
-        .querySelectorAll('.light_slider .sliders_wrapper>div');
-    let sliders_wrapper = document
-        .querySelector('.light_slider .sliders_wrapper');
+  let slides = document.querySelectorAll('.light_slider .sliders_wrapper>div');
+  let sliders_wrapper = document.querySelector(
+    '.light_slider .sliders_wrapper'
+  );
 
-    sliders_wrapper.style.width = 
-        `${slides.length * defaultOptions.slidesWidth}px`;
-    slides.forEach((el) => {
-        el.classList.add('one_slide');
-        el.style.width = `${defaultOptions.slidesWidth}px`;
-        el.style.height = `${defaultOptions.slidesHeight}px`;
-    });
+  sliders_wrapper.style.width = `${
+    slides.length * defaultOptions.slidesWidth
+  }px`;
+  slides.forEach((el) => {
+    el.classList.add('one_slide');
+    el.style.width = `${defaultOptions.slidesWidth}px`;
+    el.style.height = `${defaultOptions.slidesHeight}px`;
+  });
 }
